25282142a3d4e2ee9cbd92ecb9720df0fcc6a653
mehbark <terezi@pyrope.net>
2025-06-19 20:53:01 -0400

    bots/discord: puyo canvas

--- bots/puyo.ts
+++ bots/puyo.ts
@@ -18,7 +18,7 @@ const client = new Client({
     ],
 });
 
-client.once(Events.ClientReady, (c) => {
+client.once(Events.ClientReady, (c: Client<true>) => {
     console.log(`okay cool, ${c.user.tag}`);
 });
 
@@ -28,9 +28,33 @@ const db = await Deno.openKv();
 
 const mehbark = "354988989100589058";
 
+const square = {
+    empty: 0,
+    red: 1,
+    green: 2,
+    blue: 3,
+    yellow: 4,
+    purple: 5,
+} as const;
+
+type Square = (typeof square)[keyof typeof square];
+
+const square_names = Object.fromEntries(
+    Object.entries(square).map(([k, v]) => [v, k]),
+) as Record<Square, string>;
+
+function squareToEmoji(sq: Square): string {
+    if (sq == square.empty) return ":black_large_square:";
+    return `:${square_names[sq]}_square:`;
+}
+
 const board = {
     width: 6,
     height: 12,
+
+    inBounds(x: number, y: number): boolean {
+        return 0 <= x && x < this.width && 0 <= y && y < this.height;
+    },
 
     mapSquareIndices<T>(proc: (x: number, y: number) => T): T[] {
         const out = [];
@@ -42,30 +66,108 @@ const board = {
         return out;
     },
 
-    async init({ force = false } = {}) {
-        if (!force && (await db.get(["board"])).value != null) return;
-        await db.set(
-            ["board"],
-            this.mapSquareIndices((x, y) => ({ x, y, color: null })),
-        );
+    mapRows<T>(
+        proc: (sq: { x: number; y: number; square: Square }) => Promise<T>,
+    ): Promise<T[][]> {
+        const out = [];
+        for (let y = 0; y < this.height; y++) {
+            const row = [];
+            for (let x = 0; x < this.width; x++) {
+                row.push(
+                    db.get(["board", x, y]).then(({ value }) => {
+                        assert(
+                            typeof value == "number" && value in square_names,
+                        );
+                        return proc({ x, y, square: value as Square });
+                    }),
+                );
+            }
+            out.push(Promise.all(row));
+        }
+        return Promise.all(out);
     },
 
-    async preview(): Promise<string> {
-        return JSON.stringify((await db.get(["board"])).value);
+    async init({ force = false } = {}) {
+        if (!force && (await db.get(["board"])).value != null) return;
+        await Promise.all(this.mapSquareIndices((x, y) => {
+            return db.set(["board", x, y], square.empty);
+        }));
+    },
+
+    async status(): Promise<string> {
+        return (await this.mapRows(({ square }) =>
+            Promise.resolve(squareToEmoji(square))
+        )).map((row) => row.join("")).join("\n");
+    },
+
+    async set(
+        { x, y, sq }: { x: number; y: number; sq: Square },
+    ): Promise<boolean> {
+        if (!this.inBounds(x, y)) return false;
+        await db.set(["board", x, y], sq);
+        return true;
     },
 };
 
 await board.init();
 
+const commands: Record<string, (args: string[]) => Promise<string>> = {
+    async clear() {
+        await board.init({ force: true });
+        return "db cleared";
+    },
+    async set(args) {
+        const usage = "```\nUSAGE:\nset x y (" +
+            Object.keys(square).toSorted().join("|") + ")\n```";
+
+        if (
+            args.length != 3 || Number.isNaN(Number.parseInt(args[0])) ||
+            Number.isNaN(Number.parseInt(args[1])) || !(args[2] in square)
+        ) {
+            return usage;
+        }
+        if (
+            !(await board.set({
+                x: Number.parseInt(args[0]),
+                y: Number.parseInt(args[1]),
+                sq: square[args[2] as keyof typeof square],
+            }))
+        ) {
+            return "oob";
+        }
+
+        return board.status();
+    },
+    view() {
+        return board.status();
+    },
+    help() {
+        return Promise.resolve(
+            "commands:\n" +
+                Object.keys(commands).toSorted().map((k) =>
+                    `- ${k}${admin_commands.includes(k) ? " (admin)" : ""}`
+                ).join("\n"),
+        );
+    },
+};
+
+const admin_commands: string[] = ["clear"];
+
 client.on("messageCreate", async (message) => {
     if (message.author == id || !message.mentions.has(id)) return;
 
-    if (message.author.id == mehbark && message.content.includes("clear")) {
-        await board.init({ force: true });
-        message.reply("db cleared");
+    const is_admin = message.author.id == mehbark;
+    const args = message.content.toLowerCase().split(/\s+/).filter((arg) =>
+        !arg.includes("@")
+    );
+    if (
+        args[0] && args[0] in commands &&
+        (is_admin || !admin_commands.includes(args[0]))
+    ) {
+        message.reply((await commands[args[0]](args.slice(1))).slice(0, 2000));
     } else {
         message.reply(
-            "```json\n" + (await board.preview()).slice(0, 1900) + "\n```",
+            (await commands.help([])).slice(0, 1900),
         );
     }
 });
