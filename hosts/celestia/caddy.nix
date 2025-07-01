c85ddeff72de19707e1da6812aa46eb1cdc2d7a1
mehbark <terezi@pyrope.net>
2025-06-30 22:51:09 -0400

    hosts/celestia: cattenheimer: poc serving

--- hosts/celestia/caddy.nix
+++ hosts/celestia/caddy.nix
@@ -5,7 +5,6 @@
     globalConfig = ''
       # nginx'll manage it for now :(
       auto_https off
-      encode zstd gzip
     '';
     virtualHosts."celestia.pyrope.net:80".extraConfig = ''
       root * /srv/www
@@ -16,7 +15,17 @@
       respond "meow"
     '';
     virtualHosts."*.cattenheimer.xyz:80".extraConfig = ''
-      respond "subdomain meow"
+      handle_errors {
+        rewrite * /{err.status_code}
+        reverse_proxy https://http.cat {
+          header_up Host {upstream_hostport}
+          replace_status {err.status_code}
+        }
+      }
+
+      root * /srv/cattenheimer/{http.request.host.labels.2}
+      encode zstd gzip
+      file_server browse
     '';
   };
 }
