48c6146095cd92faf098a416c68171d99fb8c79c
mehbark <terezi@pyrope.net>
2025-06-30 22:13:53 -0400

    hosts/celestia: give up on caddy https + cattenheimer

--- hosts/celestia/caddy.nix
+++ hosts/celestia/caddy.nix
@@ -1,15 +1,22 @@
 { ... }:
 {
-  # currently nonfunctional because my vps with the public ipv4 is wack
   services.caddy = {
     enable = true;
     globalConfig = ''
+      # nginx'll manage it for now :(
       auto_https off
+      encode zstd gzip
     '';
-    virtualHosts.":80".extraConfig = ''
+    virtualHosts."celestia.pyrope.net:80".extraConfig = ''
       root * /srv/www
       encode zstd gzip
       file_server browse
+    '';
+    virtualHosts."cattenheimer.xyz:80".extraConfig = ''
+      respond "meow"
+    '';
+    virtualHosts."*.cattenheimer.xyz:80".extraConfig = ''
+      respond "subdomain meow"
     '';
   };
 }
